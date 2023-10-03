4d5af684f15c9744224c976f67df3c448821f9e2
seunggyu <seunggyujung008@gmail.com>
2023-10-03 23:41:15 +0900

    Feature: 1. seconds에 숫자가 나타나게 만들기 2. 입력된 값이 1초 간격으로 차감되기

--- js/index.js
+++ js/index.js
@@ -3,46 +3,58 @@ const minSelector = document.querySelector(".hrs-display");
 const secSelector = document.querySelector(".hrs-display");
 const hrsInput = document.getElementById("hrs-input");
 const minInput = document.getElementById("min-input");
-const secInput = document.getElementById("sec-input"); 
-const defaultBtn = document.querySelector('.default-btn');
-const defaultTxt = document.querySelector('.default-txt');
-const resetBtn = document.querySelector('.reset-btn');
+const secInput = document.getElementById("sec-input");
+const secInputDisplay = document.getElementById("sec-input-value");
+const defaultBtn = document.querySelector(".default-btn");
+const defaultTxt = document.querySelector(".default-txt");
+const resetBtn = document.querySelector(".reset-btn");
 
-const time = [parseInt(hrsInput.value), parseInt(minInput.value), parseInt(secInput.value)];
+let timer_sec;
+let timer_hrs;
+let timer_mins;
+let timer_secs;
 
+//let totalSeconds = secInput.value;
 
 /* start버튼을 누르면 start의 색깔이 바뀌고, text도 바뀐다. start => pause pause => start*/
 /* start 버튼을 누르면 입력된 타이머가 차감된다 */
-defaultBtn.addEventListener('click', function() {
-    let totalSeconds = secInput.value;
+defaultBtn.addEventListener("click", function () {
+  let totalSeconds = secInput.value;
+  let totalMins = minInput.value;
+  let totalHrs = hrsInput.value;
 
-    defaultBtn.classList.toggle("pause")
-    if(defaultTxt.innerHTML === "Start") {
-        defaultTxt.innerHTML = "Pause";
-    } else {
-        defaultTxt.innerHTML = "Start";
-    } 
+  defaultBtn.classList.toggle("pause");
+  if (defaultTxt.innerHTML === "Start") {
+    defaultTxt.innerHTML = "Pause";
+  } else if (defaultTxt.innerHTML === "Pause") {
+    defaultTxt.innerHTML = "Start";
+  }
 
+  let hrs_timer = setInterval(() => {});
 
-    setInterval(() => {
-        if(totalSeconds >= 0) {
-            totalSeconds--;
-            console.log(totalSeconds);
-        }
-    }, 1000);
-})
+  let seconds_timer = setInterval(() => {
+    totalSeconds--;
+    console.log(totalSeconds);
+    secInput.value = totalSeconds;
 
-//내일 구현할것:
-/* 
-1. 1초 단위로 초 줄이기
-2. 줄어드는 초 화면에 보이기
-3. pause 버튼 누르면 초 멈추고 start 버튼으로 바뀌기
-4. 0에 도달하면 start버튼으로 바뀌고 시간 끝났다고 alert 창 띄우기.
-*/
+    if (totalSeconds === 0) {
+      let mins_timer = setInterval(() => {
+        totalMins--;
+        minInput.value = totalMins;
+      }, 60000);
 
+      clearInterval(seconds_timer);
+      alert("타이머가 끝났습니다!");
+      secInput.value = "0";
 
+      defaultBtn.classList.remove("pause");
+      defaultTxt.innerHTML = "Start";
+    }
+  }, 1000);
+});
 
-
-
-
-
+/* 현재 문제:
+1. 버튼을 누르면 setInterval 함수가 계속 호출됨
+2. pause를 눌렀을때 숫자를 멈추는 방법을 모르겠음
+3. 시 -> 분 -> 초 순서대로 숫자가 차감되는법
+ */
